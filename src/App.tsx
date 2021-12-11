03aa8a7e827cb7fa07f1ba8e2f328d90902188a0
Sheharyar Anwar <sheharyar28@hotmail.com>
2021-12-12 03:22:51 +0500

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,117 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from "react";
+import * as data from "./dataset.json";
+import classes from "./App.module.css";
 
+interface IPerson {
+  carrier: string;
+  eventDate: string;
+  firstName: string;
+  lastName: string;
+  status: IStatus;
+}
+type IStatus = "COMPLETE" | "ERROR" | "INCOMPLETE";
 function App() {
+  const [isDrawerOpen, setIsDrawerOpen] = React.useState(false);
+  const [activeFilter, setActiceFilter] = React.useState<string[]>([
+    "COMPLETE",
+    "ERROR",
+    "INCOMPLETE",
+  ]);
+  const ref = useRef<HTMLDivElement>(null);
+  //@ts-ignore
+  const rawData: IPerson[] = data.default;
+  const onFilterChange = (filter: string) => {
+    if (activeFilter.includes(filter)) {
+      const filterIndex = activeFilter.indexOf(filter);
+      const newFilter = [...activeFilter];
+      newFilter.splice(filterIndex, 1);
+      setActiceFilter(newFilter);
+    } else {
+      setActiceFilter([...activeFilter, filter]);
+    }
+  };
+  console.log(activeFilter);
+
+  let header = ["First Name", "Last Name", "Carrier", "Status", "Date"].map(
+    (val) => {
+      return (
+        <th>
+          <div className={classes.org}>
+            {val}
+            {val === "Status" ? (
+              <>
+                <div
+                  className={classes.drawer}
+                  style={{
+                    visibility: isDrawerOpen ? "visible" : "hidden",
+                  }}
+                >
+                  {["Complete", "Incomplete", "Error"].map((val) => {
+                    let filterValue = val.toUpperCase();
+                    return (
+                      <label>
+                        <input
+                          type="checkbox"
+                          name="checkbox"
+                          value={filterValue}
+                          checked={activeFilter.includes(filterValue)}
+                          onClick={() => onFilterChange(filterValue)}
+                        />
+                        {val}
+                      </label>
+                    );
+                  })}
+                </div>
+                <div
+                  className={classes.arrow}
+                  ref={ref}
+                  onClick={() => {
+                    setIsDrawerOpen((prev) => !prev);
+                    ref!.current!.animate([{ transform: "rotate(180deg)" }], {
+                      duration: 200,
+                    });
+                  }}
+                ></div>
+              </>
+            ) : null}
+          </div>
+        </th>
+      );
+    }
+  );
+  const renderBody = () => {
+    return rawData.map(
+      ({ carrier, firstName, lastName, eventDate, status }, i) => {
+        if (activeFilter.includes(status)) {
+          return (
+            <tr key={i}>
+              <td>{firstName}</td>
+              <td>{lastName}</td>
+              <td>{carrier}</td>
+              <td>{status}</td>
+              <td>{eventDate}</td>
+              {/* <td className='opration'>
+                    <button className='button' onClick={() => removeData(id)}>Delete</button>
+                </td> */}
+            </tr>
+          );
+        } else {
+          return null;
+        }
+      }
+    );
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className={classes.bg}>
+        <div className={classes.tableWrapper}>
+          <table>
+            <thead>{header}</thead>
+            <tbody>{renderBody()}</tbody>
+          </table>
+        </div>
+      </div>
+    </>
   );
 }
 
---
+++ src/dataset.json
@@ -0,0 +1,37 @@
+[
+  {
+    "firstName": "Tim",
+    "lastName": "Jones",
+    "carrier": "Humana",
+    "status": "COMPLETE",
+    "eventDate": "2021-09-22"
+  },
+  {
+    "firstName": "Tim",
+    "lastName": "Jones",
+    "carrier": "Humana",
+    "status": "COMPLETE",
+    "eventDate": "2021-09-22"
+  },
+  {
+    "firstName": "Bill",
+    "lastName": "Jones",
+    "carrier": "Humana",
+    "status": "ERROR",
+    "eventDate": "2021-09-23"
+  },
+  {
+    "firstName": "Tim",
+    "lastName": "Blue",
+    "carrier": "Aetna",
+    "status": "INCOMPLETE",
+    "eventDate": "2021-09-25"
+  },
+  {
+    "firstName": "Jim",
+    "lastName": "Red",
+    "carrier": "UHC",
+    "status": "COMPLETE",
+    "eventDate": "2021-09-20"
+  }
+]
